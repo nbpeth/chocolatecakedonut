60c4229a27c8f6da779d762edb0d011ebb147d04
bud manstrong <nbpeth@gmail.com>
2016-12-11 12:17:28 -0600

    dark background for enhanced flavor

--- src/main/resources/public/donut.js
+++ src/main/resources/public/donut.js
@@ -4,7 +4,7 @@ var renderer = new THREE.WebGLRenderer();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 setRenderSize();
-renderer.setClearColor( 0xDDDDDD );
+renderer.setClearColor( 0x000000 );
 
 var geometry = new THREE.TorusGeometry( 8, 3, 24, 100 );
 var material = new THREE.MeshPhongMaterial({
